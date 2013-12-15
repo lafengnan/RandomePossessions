7d7dad4ff464405393b939655fe33d8467ea9901
lafengnan <panzhongbin@gmail.com>
2013-12-15 15:19:27 +0800

    Initial Commit

---
+++ RandomePossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomePossessions
+//
+//  Created by lafengnan on 13-12-15.
+//  Copyright (c) 2013年 lafengnan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
